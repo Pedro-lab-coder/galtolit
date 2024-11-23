9ac71082caa524211bfbe71d093ff3680c69fe07
ALUNO <ALUNO@LAB04-PC36>
2024-11-23 11:40:19 -0300

    My commit

---
+++ src/GalTolit.java
@@ -0,0 +1,20 @@
+
+
+
+public class GalTolit {
+
+    
+    public static void main(String[] args) {
+// declare as vaiáveis
+             double gallons=10;
+     double liters=0;
+     // adicione o cálculo aqui
+     
+     //exiba o resultado para o usuário
+     System.out.println(gallons+" gallons equals"+liters+" liters");
+     
+     
+     
+}
+    }
+    
